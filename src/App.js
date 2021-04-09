e5ddd792b7d3b548346b6808ac392b307909b54b
Saurabh Suman <sksiitb@gmail.com>
2021-04-09 19:08:06 +0530

    arrow head changes, unwanted code removed, react-arrows removed

--- src/App.js
+++ src/App.js
@@ -1,47 +1,36 @@
-import logo from "./logo.svg";
 import "./App.css";
-import Canvas from "./Canvas";
 import Actor from "./components/Actor/Actor";
-import Steps from "./components/Steps/Steps";
 import Threads from "./components/Threads/Threads";
 import data from "./utils/data.json";
 import React, { useState, useEffect } from 'react'
-import { getPosition } from "./utils/functions";
 import { Arrow } from "./components/Arrow/Arrow";
-import React_Arrows from "./components/Arrow/React_Arrows";
 import './components/Threads/thread.css';
 
 function App() {
   const [actors, setactors] = useState(data.actors);
   const [steps, setsteps] = useState(data.steps);
   const [rendered, setrendered] = useState(false)
-  const handleAddActor = (event) => {
-
-
-  };
+  // const handleAddActor = (event) => {
+  // };
 
   useEffect(() => {
     setrendered(true)
   }, [steps, actors])
 
-  const Plus = () => <button className="thread-add-btn">+</button>
-
-  const CreatArrow = () => {
+  const CreateArrow = () => {
     return steps?.map(step =>
-       <Arrow index={step.index} start={step.index + "_" + step.from} end={step.index + "_" + step.to} deltaActor={1} />
+      <Arrow index={step.index} start={step.index + "_" + step.from} end={step.index + "_" + step.to} deltaActor={1} />
     )
   }
-
-
   return (
     <div className="App">
-        <div className={"header"}></div>
+      <div className={"header"}></div>
       <ul className="actors">
         {actors.map((actor) => (
           <li id={actor.id} key={actor.id} className="">
             <Actor text={actor.name}>
-            {steps?.map(step => <Threads step={step} actor={actor.id} id={`${step.index}_${actor.id}`} active={actor.id === step.from || actor.id === step.to}/>)}
-            {/* <Threads id={`${steps.length + 1}_${actor.id}`}/> */}
+              {steps?.map(step => <Threads step={step} actor={actor.id} id={`${step.index}_${actor.id}`} active={actor.id === step.from || actor.id === step.to} />)}
+              {/* <Threads id={`${steps.length + 1}_${actor.id}`}/> */}
             </Actor>
           </li>
         ))}
@@ -52,7 +41,7 @@ function App() {
           </Actor>
         </li> */}
       </ul>
-      {rendered && <CreatArrow />}
+      {rendered && <CreateArrow />}
     </div>
   );
 }
